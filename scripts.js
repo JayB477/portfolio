0b5d5ea6bf52a1099383273de3870d4063f2a4e0
Jaylen Reed <jayreed.241@gmail.com>
2017-07-12 13:39:24 -0500

    changes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
-function contactSubmition() {
-    alert('Hellos');
+function submitcontactform(){
+     alert('Hellos');
+     var x= document.forms["contactform"]["email"].value;
+     if (x==""){
+         alert("Email must be filled out");
+         return false;
+     }else{
+         alert('thank you for sbmiting your content')
+     }
 }
