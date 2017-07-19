e24e6b3e2f4cf049d364fa79f60a6218156ce0c2
Jaylen Reed <jayreed.241@gmail.com>
2017-07-19 13:13:29 -0500

    changes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,16 @@
+var projects = [
+    {
+        "name": "Claim Academy"
+    }
+]
+
 $(document).ready(function () {
+
+    $.each(projects, function (index, project) {
+        console.log(project);
+        $('#projects').append('<a href="#">' + porject.link + '"target="_blank" ><img src="'++ '/>'+ project.name + '</a>');
+    });
+
 
     $('#contactForm').on('submit', submitcontactform)
 });
