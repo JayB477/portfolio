96a04336e205c43a20e6b4b931f2a30d6755e3f9
Jaylen Reed <jayreed.241@gmail.com>
2017-07-24 14:00:25 -0500

    changes

---
+++ src/components/navigation/navigation.component.js
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navigation',
+  templateurl: 'dist/html/navigation.html'
+})
+
+export class NavigationComponent{
+    name = 'Jaylen Reed';
+}
